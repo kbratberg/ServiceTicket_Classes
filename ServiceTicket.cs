dac455fcc856e5a3b93a1a150927cd642f8a9d62
Katie Bratberg <57917884+kbratberg@users.noreply.github.com>
2020-09-30 10:57:44 -0500

    added get/set

--- ServiceTicket.cs
+++ ServiceTicket.cs
@@ -1,7 +1,46 @@
+using System;
 using System.IO;
-namespace ServiceTicket {
+using System.Collections.Generic;
 
+namespace ServiceTickets_Classes
+{
+    class ServiceTicket
+    {
+      public UInt64 ticketId {get; set;}
 
+      string _summary;
 
+      public string summary{
+          get{
+          return this._summary;
+          }
+          set{
+              this._summary = value.IndexOf(',') != -1 ? $"\"{value}\"" : value;
+          }
+      }
 
+      public string yourName{
+          get;
+          set;
+          }
+
+        public string priority{
+          get;
+          set;
+          }
+          public string status{
+          get;
+          set;
+          }
+          public string assigned{
+          get;
+          set;
+          }
+          public List<string> employeeWatching {get; set;}
+          public string watching{
+          get;
+          set;
+          }
+
+    }
 }
--- TicketFile.cs
+++ TicketFile.cs
@@ -1,9 +1,35 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NLog.Web;
 
-namespace TicketFile {
+namespace ServiceTickets_Classes
+{
+    public class TicketFile
+    {
+          public string filePath { get; set; }
 
+          public List<string> Tickets {get; set;}
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+    
+        public TicketFile(string movieFilePath){
 
+            filePath = movieFilePath;
+            
+            try{
+                StreamReader sr = new StreamReader(filePath);
 
+               
+
+                while(!sr.EndOfStream){
+                    string line = sr.ReadLine();
+                    Tickets.Add(line);
+                }
+            } catch(){
+                
+            }
+        }
+    
+    }
 }
