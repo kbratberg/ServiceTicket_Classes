698166119258bf3077256cd498d05e48b78f1d77
Katie Bratberg <57917884+kbratberg@users.noreply.github.com>
2020-10-30 11:14:54 -0500

    Fixed EnhancementsFile

--- EnhancementsFile.cs
+++ EnhancementsFile.cs
@@ -8,38 +8,54 @@ namespace ServiceTickets_Classes
 {
     public class EnhancementsFile
     {
-          public string filePath { get; set; }
+        public string filePath { get; set; }
 
-          public List<Enhancements> Tickets {get; set;}
+        public List<Enhancements> Tickets { get; set; }
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
-    
-        public EnhancementsFile(string ticketFilePath){
+
+        public EnhancementsFile(string ticketFilePath)
+        {
             Tickets = new List<Enhancements>();
             filePath = ticketFilePath;
             Enhancements serviceTicket = new Enhancements();
-            try{
+            try
+            {
                 StreamReader sr = new StreamReader(filePath);
-                while(!sr.EndOfStream){
-                    
+                while (!sr.EndOfStream)
+                {
+
                     string line = sr.ReadLine();
-                int idx = line.IndexOf('"');
-                if(idx == -1){
-                    string[] ticketDetails = line.Split(",");
-                    
-                    serviceTicket.ticketId = UInt64.Parse(ticketDetails[0]);
-                    serviceTicket.summary = ticketDetails[1];
-                    serviceTicket.status = ticketDetails[2];
-                    serviceTicket.priority = ticketDetails[3];
-                    serviceTicket.yourName = ticketDetails[4];
-                    serviceTicket.assigned = ticketDetails[5];
-                    serviceTicket.employeeWatching = ticketDetails[6].Split('|').ToList();
-                    serviceTicket.software = ticketDetails[7];
-                    serviceTicket.cost = ticketDetails[8];
-                    serviceTicket.reason = ticketDetails[9];
-                    serviceTicket.estimate = ticketDetails[10];
-                }
-                else{
-                    serviceTicket.ticketId = UInt64.Parse(line.Substring(0, idx - 1));
+                    int idx = line.IndexOf('"');
+                    if (idx == -1)
+                    {
+                        string[] ticketDetails = line.Split(",");
+
+                        serviceTicket.ticketId = UInt64.Parse(ticketDetails[0]);
+                        
+                        serviceTicket.summary = ticketDetails[1];
+                        
+                        serviceTicket.status = ticketDetails[2];
+                        
+                        serviceTicket.priority = ticketDetails[3];
+                        
+                        serviceTicket.yourName = ticketDetails[4];
+                        
+                        serviceTicket.assigned = ticketDetails[5];
+                        
+                        serviceTicket.employeeWatching = ticketDetails[6].Split('|').ToList();
+                        
+                        serviceTicket.software = ticketDetails[7];
+                        
+                        serviceTicket.cost = ticketDetails[8];
+                        
+                        serviceTicket.reason = ticketDetails[9];
+                        
+                        serviceTicket.estimate = ticketDetails[10];
+                        
+                    }
+                    else
+                    {
+                        serviceTicket.ticketId = UInt64.Parse(line.Substring(0, idx - 1));
                         // remove movieId and first quote from string
                         line = line.Substring(idx + 1);
                         // find the next quote
@@ -68,46 +84,49 @@ namespace ServiceTickets_Classes
                         idx = line.IndexOf(',');
                         String employeesWatching = line.Substring(0, idx);
                         serviceTicket.employeeWatching = employeesWatching.Split('|').ToList();
-
-                        line =line.Substring(idx + 1);
+                        
+                        line = line.Substring(idx + 1);
                         idx = line.IndexOf(',');
                         serviceTicket.software = line.Substring(0, idx);
-
-                        line =line.Substring(idx + 1);
+                        
+                        line = line.Substring(idx + 1);
                         idx = line.IndexOf(',');
                         serviceTicket.cost = line.Substring(0, idx);
 
-                        line =line.Substring(idx + 1);
+                        line = line.Substring(idx + 1);
                         idx = line.IndexOf(',');
                         serviceTicket.reason = line.Substring(0, idx);
 
-                        line =line.Substring(idx + 1);
+                        line = line.Substring(idx + 1);
                         idx = line.IndexOf(',');
                         serviceTicket.estimate = line.Substring(0);
 
 
-                }
-                Tickets.Add(serviceTicket);
+                    }
+                    Tickets.Add(serviceTicket);
+                    
                 }
                 
                 sr.Close();
                 logger.Info("Enhancements in File: {Count}", Tickets.Count);
-            } catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 logger.Error(ex.Message);
             }
 
         }
-        
-     
-            public void AddEnhancementTicket(Enhancements serviceTicket){
-                serviceTicket.ticketId = Tickets.Max(s => s.ticketId) + 1;
 
-                StreamWriter sw = new StreamWriter(filePath, true);
-                sw.WriteLine($"{serviceTicket.ticketId},{serviceTicket.summary},{serviceTicket.status},{serviceTicket.priority},{serviceTicket.yourName},{serviceTicket.assigned},{string.Join('|', serviceTicket.employeeWatching)},{serviceTicket.software},{serviceTicket.cost},{serviceTicket.reason},{serviceTicket.estimate}");
-                sw.Close();
-                Tickets.Add(serviceTicket);
-            }
-            
+
+        public void AddEnhancementTicket(Enhancements serviceTicket)
+        {
+            serviceTicket.ticketId = Tickets.Max(s => s.ticketId) + 1;
+
+            StreamWriter sw = new StreamWriter(filePath, true);
+            sw.WriteLine($"{serviceTicket.ticketId},{serviceTicket.summary},{serviceTicket.status},{serviceTicket.priority},{serviceTicket.yourName},{serviceTicket.assigned},{string.Join('|', serviceTicket.employeeWatching)},{serviceTicket.software},{serviceTicket.cost},{serviceTicket.reason},{serviceTicket.estimate}");
+            sw.Close();
+            Tickets.Add(serviceTicket);
+        }
+
     }
 }
